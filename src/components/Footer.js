ce556703ea2399a7d7e20ccb756b08ab97815f32
Kamran Khan <kamrankhan.sde@gmail.com>
2024-01-31 20:16:00 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,4 @@
-import ACMLogo from "../assets/images/ACM.png"
+import ACMLogo from "../assets/images/ACM.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faInstagram,
@@ -6,27 +6,71 @@ import {
   faTwitter as faXTwitter,
   faLinkedin,
 } from "@fortawesome/free-brands-svg-icons";
+import { NavLink } from "react-router-dom";
 
 const Footer = () => {
   return (
     <>
-      <footer className="footer p-10 bg-gray-800 text-white mx-auto text-base-content" id="footer">
+      <footer
+        className="footer p-10 bg-gray-800 text-white mx-auto text-base-content"
+        id="footer"
+      >
         <aside>
-          <img src={ACMLogo} width={400} height={300} alt="ACM Logo"/>
+          <img src={ACMLogo} width={400} height={300} alt="ACM Logo" />
         </aside>
         <nav>
           <header className="footer-title">Quick Links</header>
-          <a className="link link-hover">Home</a>
-          <a className="link link-hover">About</a>
-          <a className="link link-hover">Events</a>
-          <a className="link link-hover">Team</a>
+          <NavLink to="/home" className="link link-hover">
+            Home
+          </NavLink>
+          <NavLink to="/about" className="link link-hover">
+            About
+          </NavLink>
+          <NavLink to="/events" className="link link-hover">
+            Events
+          </NavLink>
+          <NavLink to="/team" className="link link-hover">
+            Team
+          </NavLink>
         </nav>
         <nav>
           <header className="footer-title">Follow Us</header>
-          <a className="link link-hover"><FontAwesomeIcon icon={faFacebook} height={20} width={20} className="inline-block"/> Facebook</a>
-          <a className="link link-hover"><FontAwesomeIcon icon={faInstagram} height={20} width={20} className="inline-block"/> Instagram</a>
-          <a className="link link-hover"><FontAwesomeIcon icon={faXTwitter} height={20} width={20} className="inline-block"/> Twitter</a>
-          <a className="link link-hover"><FontAwesomeIcon icon={faLinkedin} height={20} width={20} className="inline-block"/> LinkedIn</a>
+          <a href="https://example.com" className="link link-hover">
+            <FontAwesomeIcon
+              icon={faFacebook}
+              height={20}
+              width={20}
+              className="inline-block"
+            />{" "}
+            Facebook
+          </a>
+          <a href="https://example.com" className="link link-hover">
+            <FontAwesomeIcon
+              icon={faInstagram}
+              height={20}
+              width={20}
+              className="inline-block"
+            />{" "}
+            Instagram
+          </a>
+          <a href="https://example.com" className="link link-hover">
+            <FontAwesomeIcon
+              icon={faXTwitter}
+              height={20}
+              width={20}
+              className="inline-block"
+            />{" "}
+            Twitter
+          </a>
+          <a href="https://example.com" className="link link-hover">
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              height={20}
+              width={20}
+              className="inline-block"
+            />{" "}
+            LinkedIn
+          </a>
         </nav>
       </footer>
     </>
